ef05f6e2b6cd481b107c489e92f4443f559dd9a8
Wojciech <wojciech.szyca@gmail.com>
2016-06-03 23:37:30 +0200

    Added calculator sketch in xaml

--- CSharpAcademiProject/CSharpAcademiProject/MainWindow.xaml.cs
+++ CSharpAcademiProject/CSharpAcademiProject/MainWindow.xaml.cs
@@ -22,7 +22,134 @@ namespace CSharpAcademiProject
     {
         public MainWindow()
         {
+            int buttonHeight = 50;
             InitializeComponent();
         }
+
+        private void ImieTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void buttonC_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void button7_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void button0_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void buttonCE_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void button8_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void button5_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void buttonDot_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void buttonPlusMinus_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void button9_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void button6_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void buttonEqual_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void buttonSquare_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void buttonMultiple_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void buttonSubstract_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void buttonAdd_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void buttonPercent_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void buttonDivision_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void buttonMrc_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void buttonMMinus_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void buttonMPlus_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
     }
 }
